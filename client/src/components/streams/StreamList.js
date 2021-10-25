97c6f288d18c4f5d032ba6027c647c533b3e056f
arpit <arpit.s@surya-soft.com>
2021-10-25 17:44:58 +0530

    Added Delete Functionality using reactportals

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -21,9 +21,12 @@ class StreamList extends React.Component{
                         EDIT
                     </Link>
                     
-                    <button className="ui button negative">
+                    {/* <button className="ui button negative">
                         DELETE
-                    </button>
+                    </button> */}
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+                        DELETE
+                    </Link>
                 </div>
             );
         }
@@ -36,7 +39,9 @@ class StreamList extends React.Component{
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
